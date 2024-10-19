aaba05ed8fc04e0fd51ceede9eb56486c61d9c1c
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-10-18 23:13:31 -0300

    Ajustes nos estilos do header

--- cart/index.php
+++ cart/index.php
@@ -17,15 +17,35 @@ require('./sheep_core/config.php');
 
     
     
-<!-- Topo --->
+    <!-- Topo (header) --->
 
-    <div class="header-koreshop">
-        <p class="logo">Kore Shop</p>
-        <div class="cart">
-            <p><i class="fa fa-shopping-cart"></i> 0</p>
+        <div class="header-koreshop">
+            <p class="logo">Kore Shop</p>
+            <div class="cart"><i class="fa fa-shopping-cart"></i>
+                <p>0</p>
+            </div>
         </div>
+    <!-- Final do topo (header) -->
 
-    </div>
+    <!-- Conteúdo do site (Itens) -->
+        <div class="container">
+            
+
+            <!--Inicio Produto-->
+            <div class="linha-produtos">
+                <div class="corpoProduto">
+                    <div class="imgProduto">
+                        <img src="assets/img/produto-1" alt="" class="produtoMiniatura" />
+                    </div>
+                        <div class="titulo"></div>
+                            <p>Curso PHP</p>
+                            <h2>R$ 497</h2>
+                </div>
+            </div>
+            <!--Fim Produto-->
+
+        </div>
+    <!-- Fim do conteúdo (Itens)-->
 
 
 </body>
